b7c01abf367047b907ab19af885699eeb1a697cb
BYU-JonGomez <jgomez@byupathway.edu>
2025-05-06 23:11:58 -0400

    I finished Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,15 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine(); // Read the first name from the console
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine(); // Read the last name from the console
+        Console.WriteLine(); // Add an empty line
+        Console.WriteLine($"Your name is {lastName.capitalize}, {firstName} {lastName}."); // Print the full name to the console
     }
 }
