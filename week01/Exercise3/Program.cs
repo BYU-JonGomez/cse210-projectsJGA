e9de473a2ef1637bc4f63b3eb19171fe568cb3a0
BYU-JonGomez <jgomez@byupathway.edu>
2025-05-10 14:38:13 -0400

    V2 of W1 exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,22 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is the magic number? ");
-        int magicnumber = int.Parse(Console.ReadLine());
-        Console.WriteLine("what is your guess? ");
-        int guessnumber = int.Parse(Console.ReadLine());
+        // Console.Write("What is the magic number? "); // Part 1
+        // int magicnumber = int.Parse(Console.ReadLine());
+        // Console.Write("What is your guess? "); // Part 2
+        // int guessnumber = int.Parse(Console.ReadLine());
 
-        if (magicnumber == guessnumber)
+        Random randomGenerator = new Random(); //Part 3
+        int magicnumber = randomGenerator.Next(1, 101);
+
+        int guessnumber = -1;
+
+        while (guessnumber != magicnumber)
         {
-            Console.WriteLine("You guessed it!");
-        }
-        while (magicnumber != guessnumber)
-        {
-            Console.WriteLine("what is your guess? ");
+            Console.Write("What is your guess?");
             guessnumber = int.Parse(Console.ReadLine());
-            if (magicnumber > guessnumber)
+
+            if (magicnumber == guessnumber)
             {
-                Console.WriteLine("Try higher");
+                Console.WriteLine("You guessed it!");
             }
             else if (magicnumber < guessnumber)
             {
@@ -27,9 +29,10 @@ class Program
             }
             else
             {
-                Console.WriteLine("You guessed it!");
-                break;
+                Console.WriteLine("Try higher!");
             }
+
         }
+
     }
 }
