04f3d85d00d9effc3ed3dd29052657696e606d6a
BYU-JonGomez <jgomez@byupathway.edu>
2025-05-10 14:03:36 -0400

    V1 of  W1 exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("What is the magic number? ");
+        int magicnumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("what is your guess? ");
+        int guessnumber = int.Parse(Console.ReadLine());
+
+        if (magicnumber == guessnumber)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        while (magicnumber != guessnumber)
+        {
+            Console.WriteLine("what is your guess? ");
+            guessnumber = int.Parse(Console.ReadLine());
+            if (magicnumber > guessnumber)
+            {
+                Console.WriteLine("Try higher");
+            }
+            else if (magicnumber < guessnumber)
+            {
+                Console.WriteLine("Try lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                break;
+            }
+        }
     }
 }
